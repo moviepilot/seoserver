c8db2ad3a60ba47f0c4a7028c886cb1a18231494
Giorgos Giannoutsos <contact@nuc.gr>
2013-02-01 18:29:12 +0100

    Reverted most changes to default source code, in order to retain 'forever'

---
+++ bin/seoserver.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var fs = require('fs');
+var forever = require('forever-monitor');
+
+// require our seoserver npm package
+
+program
+  .version('0.0.1')
+  .option('-p, --port <location>', 'Specifiy a port to run on')
+
+
+program
+  .command('start')
+  .description('Starts up an SeoServer on default port 3000')
+  .action(function () {
+    var child = new (forever.Monitor)(__dirname + '/../lib/seoserver.js', {
+      options: [program.port]
+    });
+    child.start();
+    console.log(__dirname, 'SeoServer successfully started');
+  });
+
+program.parse(process.argv);
+
